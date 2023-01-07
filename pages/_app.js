6a35fb5ac56f187924dc194ebaae0046b4a707ed
beverm2391 <beverman4528@gmail.com>
2023-01-07 17:13:09 -0500

    succesfully added dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes'
+import { Inter } from '@next/font/google';
+import { useState } from 'react'
+
+const inter = Inter({
+  subsets: ['latin'],
+  fallback: ['arial']
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+        :root {
+          --inter-font: ${inter.style.fontFamily};
+        }
+      `}
+      </style>
+      <ThemeProvider attribute='class'>
+          <main className={`${inter.className}`}>
+            <Component {...pageProps}/>
+          </main>
+      </ThemeProvider>
+    </>
+  )
 }
